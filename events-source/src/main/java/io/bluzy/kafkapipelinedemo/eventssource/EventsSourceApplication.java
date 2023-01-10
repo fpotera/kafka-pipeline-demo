efb80644d28d87ca31546813a11f1ec3f71efd33
Florin Potera <fpotera@hotmail.com>
2023-01-10 16:36:45 +0100

    Add idp and idp-config containers

---
+++ events-source/src/main/java/io/bluzy/kafkapipelinedemo/eventssource/EventsSourceApplication.java
@@ -0,0 +1,13 @@
+package io.bluzy.kafkapipelinedemo.eventssource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsSourceApplication.class, args);
+	}
+
+}
