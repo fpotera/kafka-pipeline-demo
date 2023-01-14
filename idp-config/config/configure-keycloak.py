fb4b5ac15a4b44231511bacf08b092f98817a000
Florin Potera <fpotera@hotmail.com>
2023-01-14 18:01:34 +0100

    fix indentation

--- idp-config/config/configure-keycloak.py
+++ idp-config/config/configure-keycloak.py
@@ -25,6 +25,7 @@ CLIENTS_PATH = '/admin/realms/master/clients'
 
 TEST_CLIENT_FILE = 'test-client.json'
 
+
 def main():
     conn = http.client.HTTPConnection(KEYCLOAK_HOST_PORT)
     access_token = get_access_token(conn)
@@ -36,44 +37,48 @@ def main():
 
     conn.close()
 
+
 def get_access_token(conn):
-    params = urllib.parse.urlencode({'client_id': CLIENT_ID, 
-                                     'username': USER, 
+    params = urllib.parse.urlencode({'client_id': CLIENT_ID,
+                                     'username': USER,
                                      'password': PASSWORD,
                                      'grant_type': GRANT_TYPE})
     headers = {'Content-type': 'application/x-www-form-urlencoded',
                'Accept': 'application/json'}
-    
+
     conn.request("POST", TOKEN_PATH, params, headers)
     response = conn.getresponse()
     print('*** access token creation result:', response.status, response.reason)
 
     data = response.read()
-    
+
     return json.loads(data)['access_token']
+
 
 def create_client(conn, access_token):
     headers = {'Content-type': 'application/json',
                'Accept': 'application/json',
-               'Authorization': 'bearer '+access_token}
+               'Authorization': 'bearer ' + access_token}
 
     with open(TEST_CLIENT_FILE) as body:
         conn.request("POST", CLIENTS_PATH, body, headers)
         response = conn.getresponse()
-        print('*** realm creation result:', response.status,  response.reason)
+        print('*** realm creation result:', response.status, response.reason)
         response.read()
+
 
 def get_clients(conn, access_token):
     headers = {'Content-type': 'application/json',
                'Accept': 'application/json',
-               'Authorization': 'bearer '+access_token}
+               'Authorization': 'bearer ' + access_token}
 
-    conn.request("GET", CLIENTS_PATH+'?clientId=TestClient', None, headers)
+    conn.request("GET", CLIENTS_PATH + '?clientId=TestClient', None, headers)
     response = conn.getresponse()
     print('*** get clients result:', response.status, response.reason)
 
     data = response.read()
     print(data)
+
 
 if __name__ == "__main__":
     main()
