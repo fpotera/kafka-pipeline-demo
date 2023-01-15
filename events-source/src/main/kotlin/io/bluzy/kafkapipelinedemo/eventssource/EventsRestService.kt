a3b584d043b68d39187e6ffef27f3e1a1762fbbc
Florin Potera <fpotera@hotmail.com>
2023-01-15 16:14:54 +0100

    add docker components

---
+++ events-source/src/main/kotlin/io/bluzy/kafkapipelinedemo/eventssource/EventsRestService.kt
@@ -0,0 +1,12 @@
+package io.bluzy.kafkapipelinedemo.eventssource
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EventsRestService {
+    @GetMapping("/events")
+    fun getEvents(): String {
+        return "Salut!!!!"
+    }
+}
